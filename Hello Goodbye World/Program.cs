0abdf6a14872c192cc7667723e3de48c0637252a
Alexander <Alex3296@yandex.ru>
2017-03-28 04:52:05 +0300

    Add project files.

---
+++ Hello Goodbye World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Goodbye_World {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
